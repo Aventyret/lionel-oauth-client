963e060dea97b982c6e0c67c111f50af88f03047
Per Arnborg <kontakt@perarnborg.se>
2022-03-14 17:29:41 +0100

    Fix tag script

--- tag.sh
+++ tag.sh
@@ -1,7 +1,7 @@
 PACKAGE_VERSION="v$(node -pe "require('./package.json')['version']")"
 
 git add .
-git commit -m "v${PACKAGE_VERSION}"
+git commit -m "${PACKAGE_VERSION}"
 git push
 
 git tag "${PACKAGE_VERSION}"
