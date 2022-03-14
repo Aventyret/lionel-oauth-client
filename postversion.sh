4d97012c7d9b449fb28bfc51162effd96b72efbb
Per Arnborg <kontakt@perarnborg.se>
2022-03-14 15:17:31 +0100

    v 0.0.12

---
+++ postversion.sh
@@ -0,0 +1,14 @@
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+git add .
+git commit -m "v$PACKAGE_VERSION"
+git push
+
+git tag $PACKAGE_VERSION
+git push --tags
+
+echo "Created tag $PACKAGE_VERSION"
