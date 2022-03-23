8d726cb6a5d0058210622a03e9e6ea61f6a907bb
Daniel Dennerkrans <dennerkrans@gmail.com>
2022-03-23 09:53:43 +0100

    properly mock local and session storage, and test if error is thrown

--- test/unit/createStorageModule.test.ts
+++ test/unit/createStorageModule.test.ts
@@ -1,15 +1,40 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import createStorageModule from '../../src/createStorageModule'
 
+const mockedStorage = window.Storage as jest.Mock<Storage>
+interface MockStorage {
+  [key: string]: string
+}
+
 describe('createStorageModule', (): void => {
+  let mockStorage: MockStorage = {}
+
+  beforeAll(() => {
+    mockedStorage.prototype.setItem = jest.fn((key, value) => {
+      mockStorage[key] = value
+    })
+    mockedStorage.prototype.getItem = jest.fn(key => mockStorage[key])
+    mockedStorage.prototype.removeItem = jest.fn(key => delete mockStorage[key])
+    mockedStorage.prototype.clear = jest.fn(() => (mockStorage = {}))
+  })
+
+  beforeEach(() => {
+    mockStorage = {}
+  })
+
+  afterAll(() => {
+    mockedStorage.prototype.setItem.mockReset()
+    mockedStorage.prototype.getItem.mockReset()
+  })
+
   it('should have a storage key that is an instance of Storage', (): void => {
     const localStorageModule = createStorageModule()
     expect(localStorageModule.storage).toBeInstanceOf(Storage)
     const sessionStorageModule = createStorageModule('session')
     expect(sessionStorageModule.storage).toBeInstanceOf(Storage)
-  })
-
-  it('should throw an error if using a non-valid storage type', (): void => {
-    expect(() => createStorageModule()).toThrow('Not a valid storage type')
   })
 
   it('should set and get value to allowed key', (): void => {
@@ -21,7 +46,7 @@ describe('createStorageModule', (): void => {
 
   it('should throw error when trying to get a non-set value', (): void => {
     const module = createStorageModule()
-    expect(module.get('accessToken')).toThrow('Value not set')
+    expect(() => module.get('accessToken')).toThrow('Value not set')
   })
 
   it('should remove value from allowed key', (): void => {
@@ -29,13 +54,13 @@ describe('createStorageModule', (): void => {
     module.set('codeVerifier', '1337')
     expect(module.get('codeVerifier')).toBeTruthy()
     module.remove('codeVerifier')
-    expect(module.get('codeVerifier')).toBeFalsy()
+    expect(() => module.get('codeVerifier')).toThrow('Value not set')
   })
 
   it('should clear storage completely', (): void => {
     const module = createStorageModule()
     module.set('idToken', '1337')
     module.clear()
-    expect(module.get('idToken')).toBeFalsy()
+    expect(() => module.get('idToken')).toThrow('Value not set')
   })
 })
