15c9a07da67f211b574fdfe107f5d04ce4a30b4d
Per Arnborg <kontakt@perarnborg.se>
2022-03-24 10:48:06 +0100

    Use it fn in create oauth client tests

--- test/unit/createOauthClient.test.ts
+++ test/unit/createOauthClient.test.ts
@@ -6,11 +6,11 @@ import createOauthClient, { OauthClient } from '../../src/createOauthClient'
 import { oauthConfig } from './test-config'
 
 describe('createOauthClient', (): void => {
-  test('returns an object when using a valid config', (): void => {
+  it('should return an object when passing a valid config', (): void => {
     const oauthClient: OauthClient = createOauthClient(oauthConfig)
     expect(typeof oauthClient).toBe('object')
   })
-  test('sets correct config attributes, icluding defaults', (): void => {
+  it('should set correct config attributes, including defaults', (): void => {
     const oauthClient: OauthClient = createOauthClient(oauthConfig)
     const clientConfig = oauthClient.getConfig()
     expect(clientConfig.issuer).toBe(oauthConfig.issuer)
@@ -21,7 +21,7 @@ describe('createOauthClient', (): void => {
     expect(clientConfig.tokenEndpoint).toBe('/token')
     expect(clientConfig.debug).toBe(false)
   })
-  test('throws an error if issuer is missing', (): void => {
+  it('should throw an error if issuer is missing', (): void => {
     const invalidConfig = {
       ...oauthConfig,
       issuer: undefined
@@ -29,7 +29,7 @@ describe('createOauthClient', (): void => {
     //@ts-expect-error throws error with invalid config
     expect(() => createOauthClient(invalidConfig)).toThrow()
   })
-  test('throws an error if clientId is missing', (): void => {
+  it('should throw an error if clientId is missing', (): void => {
     const invalidConfig = {
       ...oauthConfig,
       clientId: undefined
@@ -37,7 +37,7 @@ describe('createOauthClient', (): void => {
     //@ts-expect-error throws error with invalid config
     expect(() => createOauthClient(invalidConfig)).toThrow()
   })
-  test('throws an error if redirectUri is missing', (): void => {
+  it('should throw an error if redirectUri is missing', (): void => {
     const invalidConfig = {
       ...oauthConfig,
       redirectUri: undefined
