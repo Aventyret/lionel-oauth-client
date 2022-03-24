bc77a4814ef49cf85c6554a9fccc62dd5ba025c3
Daniel Dennerkrans <dennerkrans@gmail.com>
2022-03-24 12:01:31 +0100

    jest should ignore 'e2e' folder

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test/e2e/']
 }
