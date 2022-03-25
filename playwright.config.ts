7e56c978e8ece78a8b899c1af68bc38a0f4ce55b
Daniel Dennerkrans <dennerkrans@gmail.com>
2022-03-25 09:17:20 +0100

    added comment to clarify forbidOnly config options

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-// https://playwright.dev/
+// https://playwright.dev/docs/test-configuration
 import { PlaywrightTestConfig, devices } from '@playwright/test'
 
 const PORT = 3002
@@ -6,7 +6,7 @@ const PORT = 3002
 const config: PlaywrightTestConfig = {
   testDir: 'test/e2e',
   testIgnore: '**/unit/**',
-  forbidOnly: !!process.env.CI,
+  forbidOnly: !!process.env.CI, // Whether to exit with an error if any tests are marked as test.only (https://playwright.dev/docs/test-annotations#focus-a-test)
   retries: process.env.CI ? 2 : 0,
   use: {
     trace: 'on-first-retry'
