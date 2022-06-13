040f599cde038ee8154adeee896afc3c5b5f7d5a
Per Arnborg <kontakt@perarnborg.se>
2022-06-13 21:41:26 +0200

    Add missing exported types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,11 @@
-export { OauthClientConfig, OauthClient } from './src/create-oauth-client'
+export { OauthClientConfig, OauthClient } from './src/createOauthClient'
+
+export { SignInOptions } from './src/signIn'
+
+export { SignOutOptions } from './src/signOut'
+
+export { MetaData } from './src/metaData'
+
+export { User } from './src/user'
 
 export { StorageModuleType } from './createStorageModule'
