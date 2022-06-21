339c6575980126b2f975dbbb757fb61018eae4af
Per Arnborg <kontakt@perarnborg.se>
2022-06-21 14:34:01 +0200

    Include exported types in ts dist build

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
 export { createOauthClient } from './createOauthClient'
 export { createOidcClient } from './createOauthClient'
+
+export {
+  OauthClientConfig,
+  OauthClient,
+  ResponseMode,
+  Display
+} from './createOauthClient'
+
+export { SignInOptions, Prompt } from './signIn'
+
+export { SignOutOptions } from './signOut'
+
+export { MetaData } from './metaData'
+
+export { User } from './user'
+
+export { StorageModuleType } from './createStorageModule'
