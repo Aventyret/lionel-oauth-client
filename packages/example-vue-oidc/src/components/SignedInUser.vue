cac902106d0c9be295c746f109c2515af3dfe291
Per Arnborg <kontakt@perarnborg.se>
2023-02-03 15:46:35 +0100

    Update vue version

--- packages/example-vue-oidc/src/components/SignedInUser.vue
+++ packages/example-vue-oidc/src/components/SignedInUser.vue
@@ -10,7 +10,7 @@ const { accessToken, accessTokenExpires, user, oidcClient } =
 </script>
 
 <template>
-  <div v-if="user">
+  <div v-if="accessToken">
     <p>You are signed in as:</p>
     <div
       style="
