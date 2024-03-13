4cb2b4b7f92cfa60b74e26cae9c02d5fb1c7b6dd
Stefan van Herwijnen <stefan@devliert.info>
2024-03-13 10:31:12 +0100

    fix: fix typescript build errors

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,15 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./test/unit/setup.ts'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test/e2e/']
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test/e2e/'],
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json'
+      }
+    ]
+  }
 }
